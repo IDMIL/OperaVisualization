167c3eeb73262c1058bb4c48dad7d6a95924629f
Arden Butterfield <76457489+ArdenButterfield@users.noreply.github.com>
2026-02-04 21:40:48 -0500

    scene to bar ranges

--- site/scripts/ScoreManager.js
+++ site/scripts/ScoreManager.js
@@ -1,15 +1,15 @@
 class ScoreManager extends TimeManagerListener {
-    constructor() {
-      super();
-      this.currentPage = 5;
-    }
+  constructor() {
+    super();
+    this.currentPage = 5;
+  }
 
-    timeUpdated(scoreTime) {
-      let newPage = bar_to_page[scoreTime.bar - 1].page;
-      if (newPage !== this.currentPage) {
-        this.currentPage = newPage;
+  timeUpdated(scoreTime) {
+    let newPage = bar_to_page[scoreTime.bar - 1].page;
+    if (newPage !== this.currentPage) {
+      this.currentPage = newPage;
 
-        document.getElementById('score-viewer-image').src = "data/pages/sheet" + this.currentPage + ".png";
-      }
+      document.getElementById('score-viewer-image').src = "data/pages/sheet" + this.currentPage + ".png";
     }
+  }
 }
