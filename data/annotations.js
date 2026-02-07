c3a9bbffc936533a2b9e42ca3caf0c413a94f84b
ArdenButterfield <76457489+ArdenButterfield@users.noreply.github.com>
2026-02-07 17:51:13 +0000

    Deploying to gh-pages from @ IDMIL/OperaVisualization@f858c004b1de3d9486a7b8d92a09d265df385841 🚀

---
+++ data/annotations.js
@@ -0,0 +1,65 @@
+const annotations = [
+  {
+    time: {
+      act: 1,
+      scene: 1,
+      bar: 1,
+      beat: 1,
+    },
+    text: "Sample annotation on bar 1"
+  },
+  {
+    time: {
+      act: 1,
+      scene: 1,
+      bar: 20,
+      beat: 1,
+    },
+    text: "Another annotation on bar 20"
+  },
+  {
+    time: {
+      act: 1,
+      scene: 1,
+      bar: 70,
+      beat: 1,
+    },
+    text: "Annotation annotation on bar 70"
+  },
+  {
+    time: {
+      act: 1,
+      scene: 2,
+      bar: 300,
+      beat: 1,
+    },
+    text: "lorem ipsum dolor",
+  },
+  {
+    time: {
+      act: 1,
+      scene: 2,
+      bar: 301,
+      beat: 1,
+    },
+    text: "lorem ipsum dolor",
+  },
+  {
+    time: {
+      act: 1,
+      scene: 2,
+      bar: 301,
+      beat: 1,
+    },
+    text: "lorem ipsum dolor",
+  },
+  {
+    time: {
+      act: 1,
+      scene: 2,
+      bar: 302,
+      beat: 1,
+    },
+    text: "lorem ipsum dolor",
+  }
+]
