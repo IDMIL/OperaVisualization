46df084a11b742db02c2b3eeb24f9a857eac7fd2
Arden Butterfield <76457489+ArdenButterfield@users.noreply.github.com>
2026-02-14 19:08:40 -0500

    starting to implement highlight of current bar

--- Input Processing/parseScanscoreOutput.py
+++ Input Processing/parseScanscoreOutput.py
@@ -1,17 +1,47 @@
 import xml.etree.ElementTree as ET
 
+bar_mappings = [{}, {}, {}]
+
+actNumber = 3
+pagedir = 'Act3'
+
 with open('scanscore_xml/Act 3.xml', 'r', encoding='utf8') as f:
     tree = ET.parse(f)
 
+pageWidth = float(tree.find(".//page-width").text)
+pageHeight = float(tree.find(".//page-height").text)
+
+defaultLeftMargin = float(tree.find(".//defaults").find(".//left-margin").text)
+defaultRightMargin = float(tree.find(".//defaults").find(".//right-margin").text)
+defaultTopMargin = float(tree.find(".//defaults").find(".//top-margin").text)
+defaultBottomMargin = float(tree.find(".//defaults").find(".//bottom-margin").text)
+
+defaultStaffDistance =  float(tree.find(".//defaults").find(".//staff-distance").text)
+
+
 for child in tree.getroot():
     if child.tag == "part":
-        i = 0
+        page = 1
+        x = defaultLeftMargin
+        y = defaultTopMargin
         for measure in child:
             if measure.tag == "measure":
-                for p in measure.findall("print"):
+                measureWidth = float(measure.attrib["width"])
+
+                p = measure.find("print")
+                if p:
                     if "new-page" in p.attrib:
-                        print(measure.attrib["number"])
+                        page += 1
+                        x = defaultLeftMargin + float(p.find("system-layout").find("system-margins").find("left-margin").text)
+                        y = defaultTopMargin + float(p.find("system-layout").find("top-system-distance").text)
                     if "new-system" in p.attrib:
-                        print("--", measure.attrib["number"])
+                        x = defaultLeftMargin + float(p.find("system-layout").find("system-margins").find("left-margin").text)
+
+                bar_mappings[actNumber - 1][measure.attrib["number"]] = {
+                    "page": page, "image": f"data/pages/{pagedir}/sheet{page}.png",
+                    "x": x/pageWidth, "y": y/pageHeight, "width": measureWidth/pageWidth, "height": (pageHeight - defaultBottomMargin - y)/pageHeight}
+                x += measureWidth
             else:
                 raise(ValueError, "part should only have measures inside")
+
+print(bar_mappings)
